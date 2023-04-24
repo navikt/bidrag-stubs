272298adec65d4ef82f172f9134af11c8a08d6f5
Simen Høston <simen.naper.hoston@webstep.no>
2023-04-24 12:52:58 +0200

    Gjør batchuid om til en pathVariable for behandlingsstatus

--- src/main/kotlin/no/nav/bidrag/stubs/skatt/controller/SkattStubController.kt
+++ src/main/kotlin/no/nav/bidrag/stubs/skatt/controller/SkattStubController.kt
@@ -18,6 +18,7 @@ import no.nav.bidrag.stubs.skatt.service.SkattStubService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -240,7 +241,7 @@ class SkattStubController(
     )
     @Tag(name = "Stub for å sjekke behandlingsstatus for batch-uid")
     @ResponseBody
-    fun hentBehandlingsstatus(batchUid: String): ResponseEntity<BehandlingsstatusResponse> {
+    fun hentBehandlingsstatus(@PathVariable batchUid: String): ResponseEntity<BehandlingsstatusResponse> {
         return skattStubService.hentBehandlingsstatus(batchUid)
     }
 }
