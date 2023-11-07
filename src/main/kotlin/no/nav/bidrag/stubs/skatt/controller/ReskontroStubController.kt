306a57611777ed5a951a2dd2d4aa551e74783970
Simen Høston <simen.naper.hoston@webstep.no>
2023-11-07 09:34:26 +0100

    Fjerner deler av url

--- src/main/kotlin/no/nav/bidrag/stubs/skatt/controller/ReskontroStubController.kt
+++ src/main/kotlin/no/nav/bidrag/stubs/skatt/controller/ReskontroStubController.kt
@@ -8,11 +8,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("ekstern/skatt")
 @ProtectedWithClaims(issuer = "maskinporten", claimMap = ["scope=nav:bidrag/v1/bidragskrav"])
 class ReskontroStubController(
     private val reskontroStubService: ReskontroStubService
